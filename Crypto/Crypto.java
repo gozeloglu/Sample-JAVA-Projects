e7d3661be1a43525ec07d64af8b879f44415dc6f
gozeloglu <gozeloglu@gmail.com>
2018-02-27 22:16:47 +0300

    encryptString method added

--- Crypto/Crypto.java
+++ Crypto/Crypto.java
@@ -3,17 +3,31 @@ import java.util.*;
 public class Crypto{
 	public static void main(String[] args){
 		Scanner input = new Scanner(System.in);
+
+		System.out.print("Please write string: ");
 		String text = input.nextLine();
 
-		String newStr = normalizeText( text );
-	//	String newTxt = caesarify(newStr, 1);
-		String groupStr = groupify( newStr, 2);
-		System.out.println( groupStr );
+		System.out.print("Please enter the shift number: ");
+		int shift = input.nextInt();
+
+		System.out.print("Please enter the group size of number: ");
+		int groupSize = input.nextInt();
+
+		String result = encryptString( text, shift, groupSize );
+		System.out.println( result );
+	}
+
+	public static String encryptString( String encyrpted, int shift, int groupSize ){
+		String txt = normalizeText( encyrpted );
+		String txt2 = caesarify( txt, shift );
+		String txt3 = groupify( txt2, groupSize );
+
+		return txt3;
 	}
 
 	public static String normalizeText( String str ){
 		int i;
-		
+
 		for ( i = 0; i < str.length(); i++){
 			if ( str.substring(i, i+1).equals("?") || str.substring(i ,i+1).equals("(") || str.substring(i, i+1).equals(")")){
 				str = str.replace(str.substring(i, i+1), "");
@@ -24,17 +38,17 @@ public class Crypto{
 		str = str.replaceAll("\\W", "");
 		str = str.toUpperCase();
 		return str;
-		
+
 		}
 
 	public static String caesarify(String text, int shift){
 		int start = 0;
-		shift = shift % 26; 
-		
+		shift = shift % 26;
+
 		String result = "";
-		
+
 		int i;
-		 
+
 		for ( i = 0; i < text.length(); i++){
 			int x = (int)text.charAt(i) + shift;
 			if ( x <= 90 ){
@@ -56,16 +70,16 @@ public class Crypto{
 		String secondString = text.substring(perNumber, text.length());
 		String result = "";
 
-		if ( secondString.length() % 2 == 0 ) {
+		if ( secondString.length() % perNumber == 0 ) {
 			int i;
-			for ( i = 0; i < secondString.length(); i += 2 ){
-				result += secondString.substring(i, i+2);
+			for ( i = 0; i < secondString.length(); i += perNumber ){
+				result += secondString.substring(i, i+perNumber);
 				result += " ";
 			}
 		}else {
-			int i; 
-			for ( i = 0; i < (secondString.length()-1); i += 2){
-				result += secondString.substring(i, i+2);
+			int i;
+			for ( i = 0; i < (secondString.length()-1); i += perNumber){
+				result += secondString.substring(i, i+perNumber);
 				result += " ";
 			}
 			result += text.charAt( text.length() - 1 );
@@ -75,10 +89,4 @@ public class Crypto{
 		return firstString + " " + result;
 	}
 
-
-
-
-
 }
-
-
